ee640da4bd57d1fbaa3d2286e52c5bc9aa2f3c85
vitor pavani <vitorpavani@poli.ufrj.br>
2020-02-28 17:48:00 -0300

    add keyboard capture on main and fix some names
    Co-authored-by: Rayner Sulyak <raynersulyak@gmail.com>

--- main.py
+++ main.py
@@ -1,28 +1,100 @@
-from player import Player
-from threading import Thread
 import configparser
+from player import Player
+from pathlib import Path
+from pynput.keyboard import Key
+from pynput.keyboard import Listener
+from threading import Thread
 
+DEBUG = False
 
-def auto_detect(player):
+keys = {
+    "Key.f1" : "f1",
+    "Key.f2" : "f2",
+    "Key.f3" : "f3",
+    "Key.f4" : "f4",
+    "Key.f5" : "f5",
+    "Key.f6" : "f6",
+    "Key.f7" : "f7",
+    "Key.f8" : "f8",
+    "Key.f9" : "f9",
+    "Key.f10" : "f10",
+    "Key.f11" : "f11",
+    "Key.f12" : "f12",
+    "Key.shift" : "shift",
+    "Key.shift_r" : "shift_r",
+    "Key.ctrl_r" : "ctrl_r",
+    "Key.ctrl_l" : "ctrl_r",
+    "Key.enter" : "enter",
+    "<96>" : "num0",
+    "<97>" : "num1",
+    "<98>" : "num2",
+    "<99>" : "num3",
+    "<100>" : "num4",
+    "<101>" : "num5",
+    "<102>" : "num6",
+    "<103>" : "num7",
+    "<104>" : "num8",
+    "<105>" : "num9"
+}
+
+def get_vb_index(pyaudio_player):
     """
     auto detect VB-Cable device index
     """
-    for i in range(player.get_device_count()):
-        a = player.get_device_info_by_index(i)
+    for i in range(pyaudio_player.get_device_count()):
+        device = pyaudio_player.get_device_info_by_index(i)
 
-        if a.get("name") == "CABLE Input (VB-Audio Virtual C":
-            return a.get("index")
+        if device["name"] == 'CABLE Input (VB-Audio Virtual C':
+            return device["index"]
+    else:
+        return None
 
-def load_binds():
-    pass
+config = configparser.ConfigParser()
+config.read("config.cfg")
 
-def main():
-    player = Player()
-    vb_ind = auto_detect(player.p)
-    with player as p:
-        Thread(p.play("chirp.wav"))
-        Thread(p.play("chirp.wav",device=vb_ind))
+player = Player()
+
+if not config["DEFAULT"]["cable index"]:
+    vb_ind = get_vb_index(player.pyaudio_instance)
+else:
+    vb_ind = config.getint("DEFAULT", "cable index")
+
+sound_folder = Path(config["DEFAULT"]["sound folder"])
+f = sound_folder / "chirp.wav"
+
+
+def play(f):
+    player.play(str(f))
+    player.play(str(f),device=vb_ind)
+
+
+def on_press(key):
+    if DEBUG:
+        print('{0} pressed'.format(key))
     
+    try:
+        k = keys['{0}'.format(key)]
+    except:
+        k = '{0}'.format(key)
+    
+    if DEBUG:
+        print(k)
 
-if __name__ == "__main__":
-    main()
+    # acha o audio pra tocar
+
+    if key == Key.f1: # menos feio porem correto
+        Thread(target=play, args=(f, )).start()
+        
+def on_release(key):
+    if DEBUG:
+        print('{0} release'.format(key))
+
+    if key == Key.f10:
+        # Stop listener
+        return False
+
+
+with Listener(
+    on_press=on_press,
+    on_release=on_release) as listener:
+        listener.join()
--- player.py
+++ player.py
@@ -4,50 +4,47 @@ import sys
 
 
 class Player:
+    """ class to wrap pyaudio audio playback """    
+    
     def __init__(self):
-        """ class to wrap pyaudio audio playback """    
         self.CHUNK = 1024
-        self.p = pyaudio.PyAudio()
+        self.pyaudio_instance = pyaudio.PyAudio()
 
     """ context manager """
     def __enter__(self):
         return self
 
     def __exit__(self, type, value, traceback):
-        self.p.terminate()
+        self.pyaudio_instance.terminate()
 
-    def play(self, wf, device=None):
+    def play(self, wave_file_path, device=None):
         """ 
-        Play wf file in device
+        Play wave_file located in wave_file_path
         if no device is specified the audio will be played on default device.
         """
         
         if device is None:
-            device = self.p.get_default_output_device_info().get("index")
+            device = self.pyaudio_instance.get_default_output_device_info().get("index")
 
-        wf = wave.open(wf)
+        wave_file = wave.open(wave_file_path)
         
-        stream = self.p.open(format=self.p.get_format_from_width(wf.getsampwidth()),
-                        channels=wf.getnchannels(),
-                        rate=wf.getframerate(),
+        stream = self.pyaudio_instance.open(format=self.pyaudio_instance.get_format_from_width(wave_file.getsampwidth()),
+                        channels=wave_file.getnchannels(),
+                        rate=wave_file.getframerate(),
                         output=True,
                         output_device_index=device)
         
-        data = wf.readframes(self.CHUNK)
-
-        while data != b'':
+        data = wave_file.readframes(self.CHUNK)
+        while data:
             stream.write(data)
-            data = wf.readframes(self.CHUNK)
+            data = wave_file.readframes(self.CHUNK)
     
         stream.stop_stream()
         stream.close()
 
 """
 usage example
-
-if __name__=="__main__":
-    player = Player()
     with player as p:
         p.play("chirp.wav")
-
 """
+
